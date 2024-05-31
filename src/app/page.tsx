a4f4dc7ac8264c3cabe602e2925003067965c800
Ruth-Musasa <ruthebondo2001@gmail.com>
2024-05-31 12:54:42 +0200

    premier commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,16 @@ import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-do
 import Dashboard from "@/Components/Dashboard";
 export default function Home() {
   return (
-    <div className="bg-white h-screen flex ">
-      <div className="w-2/12">
-        <div className="h-20 bg-yellow-500"> Logo</div>
-        <Router>
-          <Dashboard />
-        </Router>
-      </div>
-      <div className="bg-gray-500 w-10/12">
+    <div>
+      <div className="h-12 bg-yellow-100"> Logo</div>
+      <div className="bg-white h-screen flex ">
+        <div className="w-2/12">
+          <Router>
+            <Dashboard />
+          </Router>
+        </div>
+        <div className="bg-gray-500 w-10/12">
+        </div>
       </div>
     </div>
   );
